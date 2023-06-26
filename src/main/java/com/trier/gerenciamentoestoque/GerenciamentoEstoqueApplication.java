044e6e3a67098b87e7e936969e72561584587d13
unknown <vini.tvf@hotmail.com>
2023-06-26 09:18:52 -0300

    First commit

---
+++ src/main/java/com/trier/gerenciamentoestoque/GerenciamentoEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.trier.gerenciamentoestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoEstoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trier/gerenciamentoestoque/GerenciamentoEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trier.gerenciamentoestoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
